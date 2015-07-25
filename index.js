6d9bb1fe102312e75f273dc2b288909c9764f564
olih <bitbucket.olivier.4aswabra@flarebyte.com>
2015-07-25 15:26:56 +0100

    Adds basic methods for next float

--- index.js
+++ index.js
@@ -1,4 +1,51 @@
 'use strict';
-module.exports = function (str) {
-  console.log(str || 'Rainbow');
+var Joi = require('joi');
+var _ = require('lodash');
+
+var schema = Joi.object().keys({
+    text: Joi.string().min(20).required(),
+    modulus: Joi.number().integer().min(2).max(127)
+});
+
+module.exports = function(cfg) {
+    Joi.assert(cfg, schema);
+    var text = cfg.text;
+    var modulus = _.isUndefined(cfg.modulus)? 100: cfg.modulus;
+    var maxIdx = Joi.number().max(cfg.text.length-1);
+    var idx = -1;
+
+    var nextCharCode = function() {
+    	idx++;
+    	Joi.assert(idx,maxIdx);
+    	return text.charCodeAt(idx);
+
+    };
+
+    var nextMaxCode = function(max) {
+    	var i = nextCharCode();
+    	return i % max;
+    };
+
+    var nextUnitRatio= function() {
+    	console.log(modulus);
+    	return nextMaxCode(modulus) / modulus;
+    };
+
+    var nextFloat = function(min, max) {
+    	Joi.assert(min, Joi.number().max(max));
+    	Joi.assert(max, Joi.number().min(min));
+    	var diff = max -  min;
+    	return (nextUnitRatio() * diff) + min;
+    	
+
+    };	
+
+    var chance = {
+        nextCharCode: nextCharCode,
+        nextMaxCode: nextMaxCode,
+        nextUnitRatio: nextUnitRatio,
+        nextFloat: nextFloat
+    };
+
+    return chance;
 };
--- test/test.js
+++ test/test.js
@@ -1,11 +1,40 @@
 /*global describe, it */
 'use strict';
-var assert = require('assert');
+var assert = require('chai').assert;
 var dazzlingChance = require('../');
 
+var cfgWilde = {
+	text: "The only difference between the saint and the sinner is that every saint has a past, and every sinner has a future. (O.Wilde)"
+};
+
 describe('dazzling-chance node module', function () {
-  it('must have at least one test', function () {
-    dazzlingChance();
-    assert(true, 'I was too lazy to write any tests. Shame on me.');
+  it('must return the next char code', function () {
+    var chance = dazzlingChance(cfgWilde);
+    assert.equal(84,chance.nextCharCode());
+    assert.equal(104,chance.nextCharCode());
+    assert.equal(101,chance.nextCharCode());
+    assert.equal(32,chance.nextCharCode());
+    assert.equal(111,chance.nextCharCode());
+    assert.equal(110,chance.nextCharCode());
+  });
+
+  it('must return the next max code', function () {
+    var chance = dazzlingChance(cfgWilde);
+    assert.equal(4,chance.nextMaxCode(10));
+    assert.equal(8,chance.nextMaxCode(16));
+  });
+
+  it('must return the next unit ratio', function () {
+    var chance = dazzlingChance(cfgWilde);
+    assert.equal(0.84,chance.nextUnitRatio());
+    assert.equal(0.04,chance.nextUnitRatio());
+  });
+
+  it('must return the next float', function () {
+    var chance = dazzlingChance(cfgWilde);
+    assert.equal(2.52,chance.nextFloat(0,3));
+    assert.equal(-9.2,chance.nextFloat(-10,10));
+    assert.equal(-0.98,chance.nextFloat(-1,1));
+    assert.equal(-0.36,chance.nextFloat(-1,1));
   });
 });
