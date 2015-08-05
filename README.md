59b25ee94074c5a736195861a857e28ac4f4c495
olih <bitbucket.olivier.4aswabra@flarebyte.com>
2015-08-05 08:28:27 +0100

    Weight works except last step
    Fibonacci is working
    
    Adds test for fibonacci descending
    
    Adds flexible next method
    
    Adds n times next
    
    Adds n high res
    
    Adds readme

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 > Number generator based on a text input with vague random properties
 
+In short, the generator produces random values that are predictable and can be repeated.
+
 
 ## Install
 
@@ -10,12 +12,137 @@ $ npm install --save dazzling-chance
 ```
 
 
-## Usage
+## Create your chance
+
+This will create a chance object with:
+* text:  an input text used for generating the numbers in a predictable way.
+* characters: a list of characters with their mapping to a number.
+* weighting (optional): a list of weights to bend the luck a little bit. 
+
 
 ```js
-var dazzling-chance = require('dazzling-chance');
+var dazzlingChance = require('dazzling-chance');
 
-dazzling-chance('Rainbow');
+       var chance = dazzlingChance({
+            text: "qwertyuopasdfghjklzxcvbnm",
+            characters: [{
+                chars: "0-9a-z",
+                integer: 0
+            }],
+            weighting: [{
+                name: "fibonacci",
+                weights: [1, 2, 3, 5, 8, 13, 21, 2]
+            }]
+
+        });
+
+```
+
+## Create a float number between min and max
+
+```
+chance.nextFloat(0, 3))
+```
+
+Or using a weight:
+
+```
+chance.nextWeightedFloat('fibonacci', 0, 3)
+```
+
+## Create an int number between min and max
+
+```
+chance.nextInt(0, 3))
+```
+
+Or using a weight:
+
+```
+chance.nextWeightedInt('fibonacci', 0, 3))
+```
+
+## Create a boolean
+
+```
+chance.nextBool()
+```
+
+Or using a weight:
+
+```
+chance.nextWeightedBool('fibonacci')
+```
+
+
+## Create a string from a given list
+
+```
+chance.nextString(['a', 'b', 'c', 'd', 'e'])
+```
+Or using a weight:
+
+```
+chance.nextWeightedString('fibonacci', ['a', 'b', 'c', 'd', 'e'])
+```
+
+## Create a number from a given list
+
+```
+chance.nextNumber([36, 47, 3, 68, 34])
+```
+Or using a weight:
+
+```
+chance.nextWeightedNumber('fibonacci', [36, 47, 3, 68, 34]), 'A')
+```
+
+## Create the next random value
+
+type can be: 'int', 'float', 'number', 'bool', 'string'
+
+```
+chance.next({
+            type: 'float',
+            weight: 'fibonacci',
+            min: 0,
+            max: 3
+        });
+```
+
+## Create an array of random values
+
+```
+chance.nextX(4, {
+            type: 'float',
+            min: 0,
+            max: 10
+        })
+
+```
+
+## Create a high resolution random value
+
+```
+chance.nextHighRes({
+            type: 'float',
+            minResolution: 1000,
+            min: 0,
+            max: 1000
+        })
+```
+
+## Create an array of high resolution random values
+
+Ex: 10 values
+
+```
+chance.nextHighResX(10, {
+            type: 'int',
+            minResolution: 1000,
+            min: 0,
+            max: 1000
+        })
 ```
 
 
--- test/test.js
+++ test/test.js
@@ -9,6 +9,10 @@ var cfgWilde = {
         chars: "0-9a-zA-Z ,().",
         integer: 0
     }],
+    weighting: [{
+        name: "fibonacci",
+        weights: [1, 2, 3, 5, 8, 13, 21, 2]
+    }]
 };
 
 describe('dazzling-chance node module', function() {
@@ -56,6 +60,46 @@ describe('dazzling-chance node module', function() {
         assert.equal(12, chance.mapping['c']);
         assert.equal(35, chance.mapping['z']);
     });
+
+    it('must return weighting', function() {
+        var chance = dazzlingChance({
+            text: "qwertyuopasdfghjklzxcvbnm",
+            characters: [{
+                chars: "0-9a-z",
+                integer: 0
+            }],
+            weighting: [{
+                name: "fibonacci",
+                weights: [1, 2, 3, 5, 8, 13, 21, 2]
+            }]
+
+        });
+        var fibonacci = chance.weighting['fibonacci'];
+        assert.equal(fibonacci.length, 36);
+        assert.deepEqual(fibonacci, [0, 0.25, 0.375, 0.438, 0.5, 0.531, 0.563, 0.594, 0.625, 0.65, 0.675, 0.7, 0.725, 0.75, 0.766, 0.781, 0.797, 0.813, 0.828, 0.844, 0.859, 0.875, 0.884, 0.893, 0.902, 0.911, 0.92, 0.929, 0.938, 0.946, 0.955, 0.964, 0.973, 0.982, 0.991, 1]);
+
+    });
+
+    it('must return weighting if descending', function() {
+        var chance = dazzlingChance({
+            text: "qwertyuopasdfghjklzxcvbnm",
+            characters: [{
+                chars: "0-9a-z",
+                integer: 0
+            }],
+            weighting: [{
+                name: "fibonacci",
+                weights: [21, 13, 8, 5, 3, 2, 1]
+            }]
+
+        });
+        var fibonacci = chance.weighting['fibonacci'];
+        assert.equal(fibonacci.length, 36);
+        assert.deepEqual(fibonacci, [0, 0.143, 0.153, 0.163, 0.174, 0.184, 0.194, 0.204, 0.214, 0.225, 0.235, 0.245, 0.255, 0.266, 0.276, 0.286, 0.302, 0.318, 0.334, 0.35, 0.365, 0.381, 0.397, 0.413, 0.429, 0.457, 0.486, 0.514, 0.543, 0.571, 0.619, 0.666, 0.714, 0.786, 0.857, 0.857]);
+
+    });
+
+
 
     it('must return mapping with two rules', function() {
         var chance = dazzlingChance({
@@ -115,5 +159,121 @@ describe('dazzling-chance node module', function() {
         assert.equal(47, chance.nextNumber([36, 47, 3, 68, 34]), 'C');
         assert.equal(34, chance.nextNumber([36, 47, 3, 68, 34]), 'D');
     });
+
+    it('must return the next weighted float', function() {
+        var chance = dazzlingChance(cfgWilde);
+        assert.equal(2.841, chance.nextWeightedFloat('fibonacci', 0, 3));
+        assert.equal(3.26, chance.nextWeightedFloat('fibonacci', -10, 10));
+        assert.equal(0.25, chance.nextWeightedFloat('fibonacci', -1, 1));
+        assert.equal(0.962, chance.nextWeightedFloat('fibonacci', -1, 1));
+    });
+
+    it('must return the next weighted int', function() {
+        var chance = dazzlingChance(cfgWilde);
+        assert.equal(3, chance.nextWeightedInt('fibonacci', 0, 3));
+        assert.equal(3, chance.nextWeightedInt('fibonacci', -10, 10));
+        assert.equal(0, chance.nextWeightedInt('fibonacci', -1, 1));
+        assert.equal(1, chance.nextWeightedInt('fibonacci', -1, 1));
+    });
+
+    it('must return the next weighted bool', function() {
+        var chance = dazzlingChance(cfgWilde);
+        assert.isTrue(chance.nextWeightedBool('fibonacci'));
+        assert.isTrue(chance.nextWeightedBool('fibonacci'));
+        assert.isTrue(chance.nextWeightedBool('fibonacci'));
+        assert.isTrue(chance.nextWeightedBool('fibonacci'));
+    });
+
+    it('must return the next weighted number', function() {
+        var chance = dazzlingChance(cfgWilde);
+        assert.equal(34, chance.nextWeightedNumber('fibonacci', [36, 47, 3, 68, 34]), 'A');
+        assert.equal(68, chance.nextWeightedNumber('fibonacci', [36, 47, 3, 68, 34]), 'B');
+        assert.equal(68, chance.nextWeightedNumber('fibonacci', [36, 47, 3, 68, 34]), 'C');
+        assert.equal(34, chance.nextWeightedNumber('fibonacci', [36, 47, 3, 68, 34]), 'D');
+    });
+
+    it('must return the next weighted string', function() {
+        var chance = dazzlingChance(cfgWilde);
+        assert.equal('e', chance.nextWeightedString('fibonacci', ['a', 'b', 'c', 'd', 'e']), 'A');
+        assert.equal('d', chance.nextWeightedString('fibonacci', ['a', 'b', 'c', 'd', 'e']), 'B');
+        assert.equal('d', chance.nextWeightedString('fibonacci', ['a', 'b', 'c', 'd', 'e']), 'C');
+        assert.equal('e', chance.nextWeightedString('fibonacci', ['a', 'b', 'c', 'd', 'e']), 'D');
+    });
+
+    it('must return the next value', function() {
+        var chance = dazzlingChance(cfgWilde);
+        assert.equal(2.841, chance.next({
+            type: 'float',
+            weight: 'fibonacci',
+            min: 0,
+            max: 3
+        }));
+        assert.equal(-5, chance.next({
+            type: 'int',
+            min: -10,
+            max: 10
+        }));
+
+        assert.equal('d', chance.next({
+            weight: 'fibonacci',
+            type: 'string',
+            list: ['a', 'b', 'c', 'd', 'e'],
+        }));
+    });
+
+    it('must return the next value n times', function() {
+        var chance = dazzlingChance(cfgWilde);
+        assert.deepEqual([8.209, 2.537, 2.09, 9.254], chance.nextX(4, {
+            type: 'float',
+            min: 0,
+            max: 10
+        }));
+
+    });
+
+    it('must return the next value high resolution', function() {
+        var chance = dazzlingChance(cfgWilde);
+        var arrInt = [];
+        var arrStr = [];
+        for (var i = 200 - 1; i >= 0; i--) {
+            arrInt[i] = i;
+            arrStr[i] = "str" + i;
+        }
+
+        assert.equal(265.98351526, chance.nextHighRes({
+            type: 'float',
+            minResolution: 1000,
+            min: 0,
+            max: 1000
+        }));
+        assert.equal(928, chance.nextHighRes({
+            type: 'int',
+            minResolution: 1000,
+            min: 0,
+            max: 1000
+        }));
+
+        assert.equal('str69', chance.nextHighRes({
+            type: 'string',
+            list: arrStr,
+        }));
+
+        assert.equal(102, chance.nextHighRes({
+            type: 'string',
+            list: arrInt,
+        }));
+    });
+
+    it('must return the next value high resolution', function() {
+        var chance = dazzlingChance(cfgWilde);
+        assert.deepEqual([266, 928, 349, 512, 208, 228, 212, 215, 184, 928], chance.nextHighResX(10, {
+            type: 'int',
+            minResolution: 1000,
+            min: 0,
+            max: 1000
+        }));
+
+    });
+
 
 });
